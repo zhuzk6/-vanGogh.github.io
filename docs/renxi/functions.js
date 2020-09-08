035e910afc539f8ebb1639a0d41a5b9763baebb2
zhuzk6 <zhuzk@asiainfo.com>
2020-09-08 15:58:33 +0800

    aaa

--- docs/renxi/functions.js
+++ docs/renxi/functions.js
@@ -57,4 +57,27 @@ function timeElapse(date){
 	}
 	var result = "第 <span class=\"digit\">" + days + "</span> 天 <span class=\"digit\">" + hours + "</span> 小时 <span class=\"digit\">" + minutes + "</span> 分钟 <span class=\"digit\">" + seconds + "</span> 秒"; 
 	$("#clock").html(result);
+
+}
+function timeElapse2(date){
+    var current = Date();
+    var seconds = (Date.parse(current) - Date.parse(date)) / 1000;
+    var days = Math.floor(seconds / (3600 * 24));
+    seconds = seconds % (3600 * 24);
+    var hours = Math.floor(seconds / 3600);
+    if (hours < 10) {
+        hours = "0" + hours;
+    }
+    seconds = seconds % 3600;
+    var minutes = Math.floor(seconds / 60);
+    if (minutes < 10) {
+        minutes = "0" + minutes;
+    }
+    seconds = seconds % 60;
+    if (seconds < 10) {
+        seconds = "0" + seconds;
+    }
+    var result = "第 <span class=\"digit\">" + days + "</span> 天 ";
+    $("#clock2").html(result);
+
 }
